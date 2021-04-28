73a937dd225c45c08352a12cb96651d789a32d76
Dzmitry Litvinovich <dmitrylitvinovich@gmail.com>
2021-04-28 19:48:43 +0300

    add files1

--- Script.sh
+++ Script.sh
@@ -5,6 +5,6 @@ cd First	#Зайти в любоую папку
 touch File1.txt File2.txt File3.txt File4.json File5.json	#Создать 5 файлов (3 txt, 2 json)
 mkdir First Second Third	#Создать 3 папки
 ls -la	# Вывести список содержимого папки
-mv /c/Users/lenovo/CourseQA/Terminal/Second/First/File1.txt /c/Users/lenovo/CourseQA/Terminal/Second/First/File2.txt /c/Users/lenovo/CourseQA/Terminal/Second/Second	#переместить любые 2 файла, которые вы создали, в любую другую папку
+mv ~/CourseQA/Terminal/Second/First/File1.txt ~/CourseQA/Terminal/Second/First/File2.txt ~/CourseQA/Terminal/Second/Second	#переместить любые 2 файла, которые вы создали, в любую другую папку
 ~
 
